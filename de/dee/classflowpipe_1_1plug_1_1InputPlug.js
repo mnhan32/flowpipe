275b6fc8035db1f36b007765e3df72fc425c3066
Travis CI <travis@travis-ci.org>
2017-04-15 17:51:46 +0000

    Deploy code docs to GitHub Pages Travis build: 7
    Commit: 85d52558438786921863de3d2e582d97c16a310e

--- de/dee/classflowpipe_1_1plug_1_1InputPlug.js
+++ de/dee/classflowpipe_1_1plug_1_1InputPlug.js
@@ -3,6 +3,7 @@ var classflowpipe_1_1plug_1_1InputPlug =
     [ "__init__", "de/dee/classflowpipe_1_1plug_1_1InputPlug.xhtml#ac775ee34451fdfa742b318538164070e", null ],
     [ "__unicode__", "de/dee/classflowpipe_1_1plug_1_1InputPlug.xhtml#af3d9f88a3b255fd2c029a2b87a251c33", null ],
     [ "connect", "de/dee/classflowpipe_1_1plug_1_1InputPlug.xhtml#a0f3e881a92d7a1b4d6d07d9e63180c98", null ],
-    [ "connections", "de/dee/classflowpipe_1_1plug_1_1InputPlug.xhtml#a05c97c0ecc629abbb582b61ed0bfc9b3", null ],
-    [ "is_dirty", "de/dee/classflowpipe_1_1plug_1_1InputPlug.xhtml#a11a064c92f179b2a2fb7751f8c86236f", null ]
+    [ "value", "de/dee/classflowpipe_1_1plug_1_1InputPlug.xhtml#afcc7a4b78ecd8fa7e713f8cfa0f51017", null ],
+    [ "is_dirty", "de/dee/classflowpipe_1_1plug_1_1InputPlug.xhtml#a11a064c92f179b2a2fb7751f8c86236f", null ],
+    [ "connections", "de/dee/classflowpipe_1_1plug_1_1InputPlug.xhtml#a05c97c0ecc629abbb582b61ed0bfc9b3", null ]
 ];
