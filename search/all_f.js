da327e85f758f1dd221ff88b490f18b76c1cc863
Travis CI <travis@travis-ci.org>
2017-09-18 08:00:33 +0000

    Deploy code docs to GitHub Pages Travis build: 59
    Commit: 7c710d488bea377df40087723a53960f79fd5e64

--- search/all_f.js
+++ search/all_f.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['testnode',['TestNode',['../d4/dfa/classflowpipe_1_1celery_1_1app_1_1TestNode.xhtml',1,'flowpipe::celery::app']]]
+  ['serialize',['serialize',['../df/d88/classflowpipe_1_1graph_1_1Graph.xhtml#a668852ee76cabc3befb339df84d9e61a',1,'flowpipe.graph.Graph.serialize()'],['../d9/d44/classflowpipe_1_1node_1_1INode.xhtml#a668852ee76cabc3befb339df84d9e61a',1,'flowpipe.node.INode.serialize()'],['../d3/dd5/classflowpipe_1_1plug_1_1IPlug.xhtml#a668852ee76cabc3befb339df84d9e61a',1,'flowpipe.plug.IPlug.serialize()']]]
 ];
