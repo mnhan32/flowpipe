e621097fd3888266ed59f3076ffd2fb8da3095e4
Travis CI <travis@travis-ci.org>
2019-07-06 20:19:52 +0000

    Deploy code docs to GitHub Pages Travis build: 143
    Commit: 4697d8a359aa48f9382135c1607cf735ea9bd878

--- html/search/classes_5.js
+++ html/search/classes_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['statsreporter',['StatsReporter',['../d0/d86/classflowpipe_1_1stats__reporter_1_1StatsReporter.xhtml',1,'flowpipe::stats_reporter']]]
+  ['statsreporter',['StatsReporter',['../d0/d86/classflowpipe_1_1stats__reporter_1_1StatsReporter.xhtml',1,'flowpipe::stats_reporter']]],
+  ['subinputplug',['SubInputPlug',['../de/d09/classflowpipe_1_1plug_1_1SubInputPlug.xhtml',1,'flowpipe::plug']]]
 ];
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['add_5fnode',['add_node',['../df/d88/classflowpipe_1_1graph_1_1Graph.xhtml#a30b923ba5ff3d1b830d8785fcb905446',1,'flowpipe::graph::Graph']]]
+  ['add_5fnode',['add_node',['../df/d88/classflowpipe_1_1graph_1_1Graph.xhtml#a30b923ba5ff3d1b830d8785fcb905446',1,'flowpipe::graph::Graph']]],
+  ['all_5finputs',['all_inputs',['../d9/d44/classflowpipe_1_1node_1_1INode.xhtml#a7a3c370940a3ea4ddcc8dfa4fc595dca',1,'flowpipe::node::INode']]]
 ];
