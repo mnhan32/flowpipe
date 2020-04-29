08ccd1c813dae5dd39757c26d82699fc2837f57c
Travis CI <travis@travis-ci.org>
2020-04-29 17:58:56 +0000

    Deploy code docs to GitHub Pages Travis build: 501
    Commit: 2003d8920c6fb825c886cc133946cf9acfe73592

--- de/d37/classflowpipe_1_1event_1_1Event.js
+++ de/d37/classflowpipe_1_1event_1_1Event.js
@@ -5,6 +5,7 @@ var classflowpipe_1_1event_1_1Event =
     [ "register", "de/d37/classflowpipe_1_1event_1_1Event.xhtml#ae915c0ec35542dd4bd3a788f573f3cee", null ],
     [ "deregister", "de/d37/classflowpipe_1_1event_1_1Event.xhtml#a4d840fa14f938fbe56d0a177af95ab56", null ],
     [ "is_registered", "de/d37/classflowpipe_1_1event_1_1Event.xhtml#a516d623cdbff79f21a9903281de82afe", null ],
+    [ "clear", "de/d37/classflowpipe_1_1event_1_1Event.xhtml#ad149341d7d849ff957baee565b19c123", null ],
     [ "name", "de/d37/classflowpipe_1_1event_1_1Event.xhtml#ab74e6bf80237ddc4109968cedc58c151", null ],
     [ "_listeners", "de/d37/classflowpipe_1_1event_1_1Event.xhtml#abb302ed5bbe43008a50899415adf5d37", null ]
 ];
